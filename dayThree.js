6ba3fd54af107d489922530a2bbec04668139ae5
Jacob Marthaller <marthaller.jr@gmail.com>
2021-12-03 03:04:41 -0800

    Initial setup

---
+++ dayThree.js
@@ -0,0 +1,17 @@
+// get data
+let array = document.getElementsByTagName("pre")[0].innerText.split("\n");
+
+// clean data
+let popped = array.pop();
+
+// part one
+
+const partOne = (arr) => {
+    let gamma = "";
+    let epsilon = "";
+    for (let i = 0; i < arr.length; i++) {
+
+    }
+
+    return parseInt(gamma, 2) * parseInt(epsilon, 2)
+}
