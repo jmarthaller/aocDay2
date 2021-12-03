4afd42946958a9d03f97b16886bc054adc9d7c58
Jacob Marthaller <marthaller.jr@gmail.com>
2021-12-03 08:26:13 -0800

    Part one done

--- dayThree.js
+++ dayThree.js
@@ -5,13 +5,26 @@ let array = document.getElementsByTagName("pre")[0].innerText.split("\n");
 let popped = array.pop();
 
 // part one
-
-const partOne = (arr) => {
-    let gamma = "";
-    let epsilon = "";
-    for (let i = 0; i < arr.length; i++) {
-
+function getGammaRate(array) {
+    const outerLength = array.length;
+    const innerLength = array[0].length;
+    const sumOfOnes = new Array(innerLength).fill(0);
+    for (let i = 0; i < outerLength; i++) {
+      for (let j = 0; j < innerLength; j++) {
+        if (array[i][j] === '1') {
+          sumOfOnes[j]++;
+        }
+      }
     }
+    return sumOfOnes.map(ones => ones > outerLength / 2 ? '1' : '0').join('');
+  }
+  
+  const gammaRate = getGammaRate(array);
+  const epsilonRate = gammaRate.split('').map(digit => String(1 - digit)).join('');
+  const gammaDecimal = parseInt(gammaRate, 2);
+  const epsilonDecimal = parseInt(epsilonRate, 2);
+  console.log('Power consumption of the submarine:', gammaDecimal * epsilonDecimal);
 
-    return parseInt(gamma, 2) * parseInt(epsilon, 2)
-}
+
+
+// part two
